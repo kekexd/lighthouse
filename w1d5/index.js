400f97ac218dcb46c2cfa7c877f8241dd9b17f79
Xia Dong <dongxiapp@gmail.com>
2019-03-08 19:30:32 +0000

    w1d5

---
+++ w1d5/index.js
@@ -0,0 +1,30 @@
+var chalk = require('chalk');
+
+// var message = 'Hello ' + chalk.yellow('world');
+// console.log(message);
+
+// Combine styled and normal strings
+console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+// Compose multiple styles using the chainable API
+console.log(chalk.blue.bgRed.bold('Hello world!'));
+
+// Pass in multiple arguments
+console.log(chalk.blue('Hello', 'World!', 'Foo', 'bar', 'biz', 'baz'));
+
+// Nest styles
+console.log(chalk.red('Hello', chalk.underline.bgBlue('world') + '!'));
+
+// Nest styles of the same type even (color, underline, background)
+console.log(chalk.green(
+  'I am a green line ' +
+  chalk.blue.underline.bold('with a blue substring') +
+  ' that becomes green again!'
+));
+
+
+const error = chalk.bold.red;
+const warning = chalk.keyword('orange');
+
+console.log(error('Error!'));
+console.log(warning('Warning!'));
