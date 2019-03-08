c6f142f37f5f7a3ca9881ea970207b87f942601a
Xia Dong <dongxiapp@gmail.com>
2019-03-08 23:07:21 +0000

    import functions

---
+++ w1d5/importList.js
@@ -0,0 +1,32 @@
+var imports = require('./makeList');
+var makeList = imports.makeList;
+var printList = imports.sortList;
+
+//var list = [];
+
+// console.log(makeList);
+// console.log(printList);
+
+//console.log('my list ' + makeList(input))
+
+//var mylist = makeList(input);
+
+
+console.log('add first number ' + makeList(12))
+console.log('add second number ' + makeList(5))
+console.log('add third number ' + makeList(9))
+console.log('add fourth number ' + makeList(1))
+console.log('add fifth number ' + makeList(98))
+
+console.log(printList())
+
+// var output = printList(mylist);
+
+// console.log('output ' + output)
+
+// console.log('add second number ' + makeList(input))
+// console.log('add third number ' + makeList(input))
+
+//console.log(list)
+
+//console.log('sortedList' + printList())
